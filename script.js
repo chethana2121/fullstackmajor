4fb228d84d1d6a73a6bc209f34843f40cc38515f
chethana2121 <93599484+chethana2121@users.noreply.github.com>
2022-12-04 22:39:24 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,474 @@
+var quiz = { "JS" : [
+	{
+		"id" : 1,
+		"question" : "In Python 3,the maximum value for an integer is 2**63 -1 is",
+		"options" : [
+			{"a": "False", 
+			 "b":"True" 
+			 }
+			],
+		"answer":"False",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 2,
+		"question" : "What is a correct syntax to output 'Hello World' in Python?",
+		"options" : [
+			{"a": "print('Hello World')", 
+			 "b":"echo('Hello World');", 
+			 "c":"p('Hello World')",
+			 "d":"echo 'Hello World'"
+			}
+			],
+		"answer":"print('Hello World')",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 3,
+		"question" : "How do you insert COMMENTS in Python code?",
+		"options" : [
+			{"a": "#This is a comment", 
+			 "b":"/*This is a comment*/", 
+			 "c":"//This is a comment"
+			}
+			],
+		"answer":"#This is a comment",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 4,
+		"question" : "Which one is NOT a legal variable name?",
+		"options" : [
+			{"a": "Myvar", 
+			 "b":"my_var",
+			 "c":"my-var",
+			 "d":"_myvar"
+			}
+			],
+		"answer":"my-var",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 5,
+		"question" : "How do you create a variable with the numeric value 5?",
+		"options" : [
+			{"a": "x = 5", 
+			 "b":"Both the other answers are correct",
+			 "c":"x = int(5)"
+			}
+			],
+		"answer":"Both the other answers are correct",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 6,
+		"question" : "What is the correct file extension for Python files?",
+		"options" : [
+			{"a": ".py", 
+			 "b":".pyth",
+			 "c":".pt",
+			 "d":".pyt"
+			}
+			],
+		"answer":".py",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 7,
+		"question" : "How do you create a variable with the floating number 2.8?",
+		"options" : [
+			{"a": "x = 2.8", 
+			 "b":"x = float(2.8)",
+			 "c":"Both the answers are correct",
+			}
+			],
+		"answer":"Both the answers are correct",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 8,
+		"question" : "What is the correct syntax to output the type of a variable or object in Python?",
+		"options" : [
+			{"a": "print(type(x))", 
+			 "b":"print(typeof(x))",
+			 "c":"print(typeof x)",
+			 "d":"print(typeOf(x))",
+			}
+			],
+		"answer":"print(type(x))",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 9,
+		"question" : "What is the correct way to create a function in Python?",
+		"options" : [
+			{"a": "function myfunction():", 
+			 "b":"def myFunction():",
+			 "c":"create myFunction():",
+			}
+			],
+		"answer":"def myFunction():",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 10,
+		"question" : "In Python, 'Hello', is the same as Hello",
+		"options" : [
+			{"a": "True", 
+			 "b":"False",
+			}
+			],
+		"answer":"False",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 11,
+		"question" : "What is a correct syntax to return the first character in a string?",
+		"options" : [
+			{"a": "x = sub('Hello', 0, 1)", 
+			 "b":"x = 'Hello'[0]",
+			 "c":"x= 'Hello' sub(0, 1)"
+			}
+			],
+		"answer":"'Hello'[0]",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 12,
+		"question" : "Which method can be used to remove any whitespace from both the beginning and the end of a string?",
+		"options" : [
+			{"a": "trim()", 
+			 "b":"ptrim()",
+			 "c":"strip()",
+			 "d":"len()"
+			}
+			],
+		"answer":"strip()",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 13,
+		"question" : "Which method can be used to return a string in upper case letters?",
+		"options" : [
+			{"a": "uppercase()", 
+			 "b":"upperCase()",
+			 "c":"upper()",
+			 "d":"toUpperCase()"
+			}
+			],
+		"answer":"upper()",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 14,
+		"question" : "Which method can be used to replace parts of a string?",
+		"options" : [
+			{"a": "replaceString()", 
+			 "b":"replace()",
+			 "c":"repl()",
+			 "d":"switch()"
+			}
+			],
+		"answer":"replace()",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 15,
+		"question" : "WhWhich operator is used to multiply numbers?",
+		"options" : [
+			{"a": "%", 
+			 "b":"*",
+			 "c":"#",
+			 "d":"x"
+			}
+			],
+		"answer":"*",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 16,
+		"question" : "Which operator can be used to compare two values?",
+		"options" : [
+			{"a": "<>", 
+			 "b":"==",
+			 "c":"=",
+			 "d":"><"
+			}
+			],
+		"answer":"==",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 17,
+		"question" : "Which of these collections defines a LIST?",
+		"options" : [
+			{"a": "('apple', 'banana', 'cherry')", 
+			 "b":"['apple', 'banana', 'cherry']",
+			 "c":"('apple', 'banana', 'cherry')",
+			 "d":"{'name': 'apple', 'color': 'green'}"
+			}
+			],
+		"answer":"['apple', 'banana', 'cherry']",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 18,
+		"question" : "Which of these collections defines a TUPLE?",
+		"options" : [
+			{"a": "('apple', 'banana', 'cherry')", 
+			 "b":"['apple', 'banana', 'cherry']",
+			 "c":"('apple', 'banana', 'cherry')",
+			 "d":"{'name': 'apple', 'color': 'green'}"
+			}
+			],
+		"answer":"('apple', 'banana', 'cherry')",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 19,
+		"question" : "Which of these collections defines a SET?",
+		"options" : [
+			{"a": "('apple', 'banana', 'cherry')", 
+			 "b":"['apple', 'banana', 'cherry']",
+			 "c":"('apple', 'banana', 'cherry')",
+			 "d":"{'name': 'apple', 'color': 'green'}"
+			}
+			],
+		"answer":"{'apple', 'banana', 'cherry'}",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 20,
+		"question" : "Which of these collections defines a DICTIONARY?",
+		"options" : [
+			{"a": "('apple', 'banana', 'cherry')", 
+			 "b":"['apple', 'banana', 'cherry']",
+			 "c":"('apple', 'banana', 'cherry')",
+			 "d":"{'name': 'apple', 'color': 'green'}"
+			}
+			],
+		"answer":"{'name': 'apple','color': 'green'}",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 21,
+		"question" : "Which collection is ordered, changeable, and allows duplicate members?",
+		"options" : [
+			{"a": "TUPLE", 
+			 "b":"DICTIONARY",
+			 "c":"LIST",
+			 "d":"SET"
+			}
+			],
+		"answer":"LIST",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 22,
+		"question" : "Which collection does not allow duplicate members?",
+		"options" : [
+			{"a": "TUPLE", 
+			 "b":"SET",
+			 "c":"LIST"
+			}
+			],
+		"answer":"SET",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 23,
+		"question" : "How do you start writing an if statement in Python?",
+		"options" : [
+			{"a": "if x > y:", 
+			 "b":"if x > y then:",
+			 "c":"if (x > y) "
+			}
+			],
+		"answer":"if x > y:",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 24,
+		"question" : "How do you start writing a while loop in Python?",
+		"options" : [
+			{"a": "while x > y {", 
+			 "b":"x > y while {",
+			 "c":"while (x > y)",
+			 "d":"while x > y:"
+			}
+			],
+		"answer":"while x > y:",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 25,
+		"question" : "How do you start writing a for loop in Python?",
+		"options" : [
+			{"a": "for x > y:", 
+			 "b":"for x in y:",
+			 "c":"for each x in y:"
+			}
+			],
+		"answer":"for x > y:",
+		"score":0,
+		"status": ""
+	}
+	]
+}
+
+
+
+var quizApp = function() {
+
+	this.score = 0;		
+	this.qno = 1;
+	this.currentque = 0;
+	var totalque = quiz.JS.length;
+
+	
+	this.displayQuiz = function(cque) {
+		this.currentque = cque;
+		if(this.currentque <  totalque) {
+			$("#tque").html(totalque);
+			$("#previous").attr("disabled", false);
+			$("#next").attr("disabled", false);
+			$("#qid").html(quiz.JS[this.currentque].id + '.');
+	
+			
+			$("#question").html(quiz.JS[this.currentque].question);	
+			 $("#question-options").html("");
+			for (var key in quiz.JS[this.currentque].options[0]) {
+			  if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+		
+				$("#question-options").append(
+					"<div class='form-check option-block'>" +
+					"<label class='form-check-label'>" +
+							  "<input type='radio' class='form-check-input' name='option'   id='q"+key+"' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+								  quiz.JS[this.currentque].options[0][key] +
+							 "</span></label>"
+				);
+			  }
+			}
+		}
+		if(this.currentque <= 0) {
+			$("#previous").attr("disabled", true);	
+		}
+		if(this.currentque >= totalque) {
+				$('#next').attr('disabled', true);
+				for(var i = 0; i < totalque; i++) {
+					this.score = this.score + quiz.JS[i].score;
+				}
+			return this.showResult(this.score);	
+		}
+	}
+	
+	this.showResult = function(scr) {
+		$("#result").addClass('result');
+		$("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr  + '/' + totalque + "</h1>");
+		for(var j = 0; j < totalque; j++) {
+			var res;
+			if(quiz.JS[j].score == 0) {
+					res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+			} else {
+				res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+			}
+			$("#result").append(
+			'<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+			'<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+			'<div class="last-row"><b>Score:</b> &nbsp;' + res +
+			
+			'</div>' 
+			
+			);
+			
+		}
+	}
+	
+	this.checkAnswer = function(option) {
+		var answer = quiz.JS[this.currentque].answer;
+		option = option.replace(/\</g,"&lt;")   //for <
+		option = option.replace(/\>/g,"&gt;")   //for >
+		option = option.replace(/"/g, "&quot;")
+
+		if(option ==  quiz.JS[this.currentque].answer) {
+			if(quiz.JS[this.currentque].score == "") {
+				quiz.JS[this.currentque].score = 1;
+				quiz.JS[this.currentque].status = "correct";
+		}
+		} else {
+			quiz.JS[this.currentque].status = "wrong";
+		}
+		
+	}	
+	 
+	this.changeQuestion = function(cque) {
+			this.currentque = this.currentque + cque;
+			this.displayQuiz(this.currentque);	
+			
+	}
+	
+}
+
+
+var jsq = new quizApp();
+
+var selectedopt;
+	$(document).ready(function() {
+			jsq.displayQuiz(0);		
+		
+	$('#question-options').on('change', 'input[type=radio][name=option]', function(e) {
+
+			//var radio = $(this).find('input:radio');
+			$(this).prop("checked", true);
+				selectedopt = $(this).val();
+		});
+		
+			
+			 
+	});
+
+	
+	
+	
+	$('#next').click(function(e) {
+			e.preventDefault();
+			if(selectedopt) {
+				jsq.checkAnswer(selectedopt);
+			}
+			jsq.changeQuestion(1);
+	});	
+	
+	$('#previous').click(function(e) {
+		e.preventDefault();
+		if(selectedopt) {
+			jsq.checkAnswer(selectedopt);
+		}
+			jsq.changeQuestion(-1);
+	});	
+
+
+
